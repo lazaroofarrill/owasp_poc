188e3cf2eb0c68e3af832c9f75e5a4867720660d
lazaroofarrill <lazaroofarrill@gmail.com>
2022-07-13 03:40:20 -0400

    adding data source

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ModulesModule } from './modules/modules.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppDataSource } from './modules/common/infra/typeorm/data-source'
 
 @Module({
-  imports: [ModulesModule],
+  imports: [
+    ModulesModule,
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
